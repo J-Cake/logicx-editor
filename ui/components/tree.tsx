7680bc3be08a7d9fc525806c2467946cb2b3900e
jcake <jakieschneider13@gmail.com>
2022-06-22 04:46:28 +1000

    Keyboard UI

--- ui/components/tree.tsx
+++ ui/components/tree.tsx
@@ -5,26 +5,51 @@ export function TreeNode(props: { heading: string, children: React.ReactNode, on
 }
 
 type Props = { children: React.ReactElement<{ heading: string }>[] };
-type State = { list: { heading: string, collapsed: boolean, body: React.ReactElement<{ heading: string }> }[] };
+type State = { list: { heading: string, collapsed: boolean, body: React.ReactElement<{ heading: string }>, ref: React.RefObject<HTMLDivElement> }[], active: number };
 export default class Tree extends React.Component<Props, State> {
 
-    state: State = { list: [] };
+    state: State = { list: [], active: 0 };
 
     constructor(props: Props) {
         super(props);
 
-        this.state = { list: props.children.map(i => ({ heading: i.props.heading, collapsed: true, body: i })) };
+        this.state = {
+            list: props.children.map(i => ({
+                heading: i.props.heading,
+                collapsed: true,
+                body: i,
+                ref: React.createRef()
+            })),
+            active: -1
+        };
     }
 
+    private onChange(a: number) {
+        this.setState(prev => ({
+            list: [...prev.list.slice(0, a), { ...prev.list[a], collapsed: !prev.list[a].collapsed }, ...prev.list.slice(a + 1)]
+        }))
+    }
+
+    // private changeFocus(key: string) {
+    //     if (!['ArrowUp', 'ArrowDown'].includes(key))
+    //         return;
+
+    //     if (this.state.active <= -1) {
+    //         this.state.list[0].ref.current?.focus();
+    //         this.setState({active: 0});
+    //     }
+
+    //     const index = this.state.active + (key == 'ArrowUp' ? -1 : 1);
+    //     if (this.state.list[index]) {
+    //         this.state.list[index].ref.current?.focus();
+    //         this.setState({ active: index });
+    //     }
+    // }
+
     render() {
-        return <ul className='tree logicx-widget'>
-            {this.state.list.map((i, a) => <li className={`tree-item logicx-widget ${i.collapsed ? 'collapsed' : 'expanded'}`} key={`tree-item${i.heading}`}>
-                <label> {/*TODO: Change to tabindex based aproach*/}
-                    <input type="checkbox" checked={!this.state.list[a].collapsed} onChange={() => this.setState(prev => ({
-                        list: [...prev.list.slice(0, a), { ...prev.list[a], collapsed: !prev.list[a].collapsed }, ...prev.list.slice(a + 1)]
-                    }))} />
-                    {i.heading}
-                </label>
+        return <ul className='tree logicx-widget'> {/* tabIndex={0} onKeyUp={e => this.changeFocus(e.key)} onBlur={() => this.setState({ active: -1 })} */}
+             {this.state.list.map((i, a) => <li className={`tree-item logicx-widget ${i.collapsed ? 'collapsed' : 'expanded'}`} key={`tree-item${i.heading}`}>
+                <div tabIndex={0} ref={this.state.list[a].ref!} onClick={() => this.onChange(a)} onKeyUp={e => ['Enter', ' '].includes(e.key) && this.onChange(a)}>{i.heading}</div>
 
                 <div>
                     {i.collapsed ? null : i.body}
